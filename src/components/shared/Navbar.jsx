031478c142875537c171e79b8d20d112daaa8066
sheikhzim2004-del <sheikhzim2004@gmail.com>
2026-05-15 01:40:47 +0600

    finaly login issue fixed

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -62,15 +62,14 @@ export default function Navbar() {
               height={300}
             />
           </div>
-          {/* mobile device e  */}
-          <div className="md:hidden">
+          {/* mobile device  */}
+          { !user && <div className="md:hidden">
             <Link href="/login">
               <button className="btn px-5 py-2 rounded-xl border border-gray-300 hover:text-blue-500 cursor-pointer transition font-medium">
                 Login
               </button>
             </Link>
-
-          </div>
+          </div>}
 
           {/* Desktop Menu */}
           <div className="hidden md:flex items-center gap-8">
